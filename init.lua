6eea864a5b8f706899fb8b63f66369e0cd81df11
Oldemar Jesus <oldemego@gmail.com>
2024-04-27 12:35:09 +0000

    initial state

---
+++ init.lua
@@ -0,0 +1,54 @@
+-- setting vim customizations
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.g.mapleader = " "
+
+-- install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  -- install catppuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- install telescope
+  {
+   'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'}
+}
+local opts = {}
+
+-- setting lazy.nvim
+require("lazy").setup(plugins, opts)
+
+-- setting up catppuccin
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+-- setting up telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- setting up treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = { "lua", "javascript" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
